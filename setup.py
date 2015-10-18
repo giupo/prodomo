ded8fdcd30f8f6360ee9067b4747cf5ec1025550
Giuseppe Acito <giuseppe.acito@gmail.com>
2015-10-18 08:22:13 +0200

    faccio tracciare la versione di package.json a bumpversion

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ test_requirements = [
 
 setup(
     name='prodomo',
-    version='0.0.1',
+    version='0.0.2',
     description='Simple domotic api',
     long_description=readme + '\n\n' + history,
     author='Giuseppe Acito',
